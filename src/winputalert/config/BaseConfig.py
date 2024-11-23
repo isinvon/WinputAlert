450bc57f2f90aee2299b53733a4ae079dc48d938
sinvon <2021469084@qq.com>
2024-11-23 17:23:22 +0800

    fix(BaseConfig.py): 对配置文件的读取进行优化，若用户目录下配置文件不存在，则读取程序内部默认配置文件

--- src/winputalert/config/BaseConfig.py
+++ src/winputalert/config/BaseConfig.py
@@ -1,23 +1,51 @@
 import configparser
 import os
+from platformdirs import user_data_dir
 
 
 class BaseConfig:
     """
     基础配置类，用于读取配置文件
     """
+    # def get_config_path():
+    #     """
+    #     获取配置文件路径
+
+    #     see: https://blog.csdn.net/a772304419/article/details/139993938
+    #     """
+    #     return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini')
+    @staticmethod
     def get_config_path():
-        """ 
+        r""" 
         获取配置文件路径
-
-        see: https://blog.csdn.net/a772304419/article/details/139993938
+        这个方法会返回用户特定的配置文件路径，即 
+        C:\Users\<username>\AppData\Roaming\WinputAlert\config.ini
+        如果该路径下没有配置文件，则回退到当前脚本同级目录下的config.ini。
         """
-        return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini')
+        app_name = "WinputAlert"
+        app_author = os.getlogin()  # 获取当前登录的用户名
 
-    def __init__(self, config_file=get_config_path()):
+        # 获取用户数据目录路径
+        user_data_dir_path = user_data_dir(
+            appname=app_name, appauthor=app_author)
+
+        # 用户数据目录下的配置文件路径
+        config_path = os.path.join(user_data_dir_path, "config.ini")
+
+        # 如果用户数据目录下的配置文件不存在，则使用当前脚本同级目录下的config.ini
+        if not os.path.isfile(config_path):
+            config_path = os.path.join(os.path.dirname(
+                os.path.abspath(__file__)), "config.ini")
+
+        return config_path
+
+    def __init__(self, config_file=None):
         """ 
-        千万不要用config.ini，因为会找不到，写config/config.ini就可以了,或者./config/config.ini
+        如果传入的config_file为None，则使用默认的get_config_path方法获取配置文件路径
         """
+        if config_file is None:
+            config_file = self.get_config_path()
+
         self.config = configparser.ConfigParser()
         self.config_file = config_file  # 配置文件路径
         self.read_config()
@@ -49,14 +77,6 @@ class BaseConfig:
         :param section: 配置文件中的节名，如 [gui]、[keyboard]等
         :param option: 节内的具体选项名，如width、font_size等
         :param value: 要设置的新值
-
-        示例用法：
-        
-            # 创建BaseConfig实例
-            config = BaseConfig()
-
-            # 设置 [gui] 节下的width选项的值为150
-            config.set_option('gui', 'width', '150')
         """
         if not self.config.has_section(section):
             self.config.add_section(section)
@@ -65,19 +85,10 @@ class BaseConfig:
 
     def batch_update_values(self, section, values):
         """
-        批量更新指定节（section）下的多个选项（option）的值，并更新配置文件(允许传入任何类型，最后都将转化为str然后写入ini文件)
+        批量更新指定节（section）下的多个选项（option）的值，并更新配置文件
 
         :param section: 配置文件中的节名，如 [gui]、[keyboard]等
         :param values: 一个字典，包含要更新的选项及其新值
-
-        示例用法：
-        
-            创建BaseConfig实例
-            config = BaseConfig()
-
-            批量更新 [gui] 节下的width和height选项的值
-            values_to_update = {'width': 150, 'height': 100}
-            config.batch_update_values('gui', values_to_update)
         """
         if not self.config.has_section(section):
             self.config.add_section(section)
